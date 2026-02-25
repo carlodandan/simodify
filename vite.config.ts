b4c18ce587e631f5fe652d80aeaf55681d47186c
Carlo Dandan <carlodandan.personal@proton.me>
2026-02-25 17:31:13 +0800

    feat: switch to vite's sitemap.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
@@ -14,8 +15,24 @@ export default defineConfig({
       registerType: 'autoUpdate',
       workbox: {
         globPatterns: [
-          '**/*.{js,css,html,ico,png,jpg,jpeg,webp,svg,woff,woff2,ttf,eot}']
+          '**/*.{js,css,html,ico,png,jpg,jpeg,webp,svg,woff,woff2,ttf,eot,txt,xml}']
       }
+    }),
+    Sitemap({
+      hostname: 'https://carlodandan.pages.dev/',
+      dynamicRoutes: [
+        '/projects',
+        '/about',
+        '/contact'
+      ],
+      readable: true,
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+          crawlDelay: 5,
+        },
+      ]
     }),
     tailwindcss(),
   ],
